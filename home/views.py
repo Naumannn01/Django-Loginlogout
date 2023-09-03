a7a721f443ded669392ab371644df5b23fda5f9a
Shaikh Mohammed Nauman <115541777+Naumannn01@users.noreply.github.com>
2023-09-03 14:01:15 +0530

    views.py

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 
 from django.shortcuts import render,redirect, HttpResponse
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,auth
 from django.contrib.auth import logout,authenticate,login
+from django.contrib import messages
 
 def index(request):
      if  request.user.is_anonymous:
@@ -12,9 +13,11 @@ def loginUser(request):
    if request.method=="POST":
         username=request.POST.get('username')
         password=request.POST.get('password')
-        # print(username,password)
         user = authenticate(username=username, password=password)
-        if user is not None:
+        if user is None:
+            messages.info(request,'Credentials Invalid')
+            return redirect("/login")   
+        elif user is not None:
             login(request,user)
             return redirect("/")   
         else:
@@ -26,15 +29,28 @@ def logoutUser(request):
     return redirect("/login")
 
 def createUser(request):
+   
     if request.method=='POST':
         username=request.POST.get('username')
         email=request.POST.get('email')
         pass1=request.POST.get('password')
         pass2=request.POST.get('conpass')
-        # print(username,email,pass1)
-        if pass1!=pass2:
-            return HttpResponse("your passwords does not match")
-        new_user=User.objects.create_user(username,email,pass1)
-        new_user.save()
-        return redirect('/login')
-    return render(request,'signup.html')
+
+        if pass1 == pass2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already Taken')
+                return redirect('/signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('/signup')
+            else:
+                new_user= User.objects.create_user(username=username, email=email, password=pass1)
+                new_user.save()
+                user_login = auth.authenticate(username=username, password=pass1)
+                auth.login(request, user_login)
+                return redirect("/") 
+        else:
+            messages.info(request, 'Your password does not match')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
